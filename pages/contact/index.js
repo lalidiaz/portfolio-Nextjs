cf98cb6ce8175759507030dfbf7a17d0a4bd4a06
Lali <lauradiaz1586@gmail.com>
2021-02-06 14:55:45 +0400

    add responsive  fixes to contact page

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -49,43 +49,44 @@ export default function Contact() {
               <br />
               Let's connect!
             </p>
+
+            <motion.div variants={boxVariants} initial="out" animate="in">
+              <div className={styles.social}>
+                <motion.span variants={iconVariants}>
+                  <a href="mailto:contact@lauradiaz.dev" className={styles.a}>
+                    <HiOutlineMailOpen size={22} />
+                    <span className={styles.span}>contact@lauradiaz.dev</span>
+                  </a>
+                </motion.span>
+                <motion.span variants={iconVariants}>
+                  <a
+                    href="https://github.com/lalidiaz"
+                    target="_blank"
+                    className={styles.a}
+                  >
+                    <GoMarkGithub size={22} />
+                    <span className={styles.span}>Github</span>
+                  </a>
+                </motion.span>
+                <motion.span variants={iconVariants}>
+                  <a className={styles.a} href="/LauraDiazCV.pdf" download>
+                    <IoDocumentAttachOutline size={22} />
+                    <span className={styles.span}>CV</span>
+                  </a>
+                </motion.span>
+                <motion.span variants={iconVariants}>
+                  <a
+                    href="https://www.linkedin.com/in/laura-diaz-a661a617a/"
+                    target="_blank"
+                    className={styles.a}
+                  >
+                    <IoLogoLinkedin size={22} />
+                    <span className={styles.span}>Linkedin</span>
+                  </a>
+                </motion.span>
+              </div>
+            </motion.div>
           </div>
-          <motion.div variants={boxVariants} initial="out" animate="in">
-            <div className={styles.social}>
-              <motion.span variants={iconVariants}>
-                <a href="mailto:contact@lauradiaz.dev" className={styles.a}>
-                  <HiOutlineMailOpen />
-                  <span className={styles.span}>contact@lauradiaz.dev</span>
-                </a>
-              </motion.span>
-              <motion.span variants={iconVariants}>
-                <a
-                  href="https://github.com/lalidiaz"
-                  target="_blank"
-                  className={styles.a}
-                >
-                  <GoMarkGithub />
-                  <span className={styles.span}>Github</span>
-                </a>
-              </motion.span>
-              <motion.span variants={iconVariants}>
-                <a className={styles.a} href="/LauraDiazCV.pdf" download>
-                  <IoDocumentAttachOutline />
-                  <span className={styles.span}>CV</span>
-                </a>
-              </motion.span>
-              <motion.span variants={iconVariants}>
-                <a
-                  href="https://www.linkedin.com/in/laura-diaz-a661a617a/"
-                  target="_blank"
-                  className={styles.a}
-                >
-                  <IoLogoLinkedin />
-                  <span className={styles.span}>Linkedin</span>
-                </a>
-              </motion.span>
-            </div>
-          </motion.div>
         </div>
       </motion.div>
     </>
