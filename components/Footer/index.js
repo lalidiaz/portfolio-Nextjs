5b015c6b638e536edb7ede9a391e0be1d226a0d1
lauradiaz <lauradiaz1586@gmail.com>
2021-05-16 22:56:23 +0400

    changed email in footer component

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,18 +1,22 @@
-import styles from './footer.module.scss';
-import { FaHeart } from 'react-icons/fa';
+import styles from "./footer.module.scss";
+import { FaHeart } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <footer className={styles.footer} >
-      <a className={styles.a} href="mailto:contact@lauradiaz.dev">
-        contact@lauradiaz.dev
+    <footer className={styles.footer}>
+      <a
+        className={styles.a}
+        href="mailto:lauradiaz1586@gmail.com"
+        target="_blank"
+      >
+        lauradiaz1586@gmail.com
       </a>
       <div>
-        Made with{' '}
+        Made with{" "}
         <span>
-          {' '}
-          <FaHeart />{' '}
-        </span>{' '}
+          {" "}
+          <FaHeart />{" "}
+        </span>{" "}
         by Laura.
       </div>
     </footer>
